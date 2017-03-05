c82a5b5167f4928f9c0948920c23e278bb761b2e
Idaliya <idaliya.taisina@gmail.com>
2017-03-05 21:14:28 +0800

    refactoring

--- 1.1/app.js
+++ 1.1/app.js
@@ -2,7 +2,10 @@ var firstWord = prompt('Enter first word', '');
 var secondWord = prompt('Enter second word', '');
 
 function sortWord(word) {
-  return word.toLowerCase().split('').sort().join('');
+  if (word) {
+    return word.toLowerCase().split('').sort().join('');
+  }
+  return '';
 }
 
 const areWordsAnagrams = sortWord(firstWord) === sortWord(secondWord);
